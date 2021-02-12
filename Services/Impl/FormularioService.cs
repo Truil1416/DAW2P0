e52caa4642fc7c838bf105ea651b58067d19a979
78595225Q <truil1416jubera@gmail.com>
2021-02-12 13:54:28 +0000

    Cutre pero funcional.

--- Services/Impl/FormularioService.cs
+++ Services/Impl/FormularioService.cs
@@ -1,8 +1,11 @@
 ﻿using DAW2P0.Business;
 using DAW2P0.Services.Api;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DAW2P0.Services.Impl
@@ -18,6 +21,42 @@ namespace DAW2P0.Services.Impl
         public void guardarFormulario(Formulario formulario)
         {
             //Guardamos el formulario
+            //Soy un bruto y lo voy ha hacer leyendo un txt con jsons objects, agregando mi obj al json y escribiendo el txt entero de nuevo.
+            //para salir del paso.
+            string path = @"Persistance\ArchivosPersistentes\Formularios.txt";
+            //string path = @"C:\Users\78595225Q\source\repos\DAW2P0\Persistance\ArchivosPersistentes\Formularios.txt";
+            try {
+                if (File.Exists(path))
+                {
+                    string contenido = "";
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        contenido = sr.ReadToEnd();
+                    }
+                    List<Formulario> formularios = JsonConvert.DeserializeObject<List<Formulario>>(contenido);
+                    if (formularios == null) { formularios = new List<Formulario>(); }
+                    formularios.Add(formulario);
+                    using (FileStream fs = File.Create(path))
+                    {
+                        Byte[] datos = new UTF8Encoding(true).GetBytes(JsonConvert.SerializeObject(formularios));
+                        fs.Write(datos,0,datos.Length);
+                    }
+                }
+                else
+                {
+                    List<Formulario> formularios = new List<Formulario>();
+                    formularios.Add(formulario);
+                    using (FileStream fs = File.Create(path))
+                    {
+                        Byte[] datos = new UTF8Encoding(true).GetBytes(JsonConvert.SerializeObject(formularios));
+                        fs.Write(datos, 0, datos.Length);
+                    }
+                }
+            } 
+            catch
+            {
+
+            }
         }
 
     }
--- Services/Impl/UsuarioService.cs
+++ Services/Impl/UsuarioService.cs
@@ -1,8 +1,11 @@
 ﻿using DAW2P0.Business;
 using DAW2P0.Services.Api;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DAW2P0.Services.Impl
@@ -18,7 +21,34 @@ namespace DAW2P0.Services.Impl
         public bool Autenticar(Usuario usuario)
         {
             //acceso a la base de datos y comprobar las credenciales.
-            return ((usuario.password == "1234" && usuario.user == "admin")|| (usuario.password == "pepe" && usuario.user == "pepe"));
+            //return ((usuario.password == "1234" && usuario.user == "admin")|| (usuario.password == "pepe" && usuario.user == "pepe"));
+            string path = @"Persistance\ArchivosPersistentes\Usuarios.txt";
+            //string path = @"C:\Users\78595225Q\source\repos\DAW2P0\Persistance\ArchivosPersistentes\Formularios.txt";
+            try
+            {
+                if (File.Exists(path))
+                {
+                    string contenido = "";
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        contenido = sr.ReadToEnd();
+                    }
+                    List<Usuario> usuarios = JsonConvert.DeserializeObject<List<Usuario>>(contenido);
+                    if (usuarios == null) { return false; }
+                    bool aux = false;
+                    usuarios.ForEach(e => { if (e.password == usuario.password && e.user == usuario.user) { aux = true; } });
+                    return aux;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
         }
     }
 }
