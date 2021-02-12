f5fe678571cd074880f9bb38ab83c358a92bf36e
78595225Q <truil1416jubera@gmail.com>
2021-02-12 08:44:11 +0000

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DAW2P0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAW2P0.Models
+{
+    public class Usuario
+    {
+        public String user { get; set; }
+        public String password { get; set; }
+    }
+}
